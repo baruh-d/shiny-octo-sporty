d34d1eed24cfc0d8a915352f6e85cc90cc06d6c9
baruh-d <barudaniel6@gmail.com>
2025-04-14 15:03:59 +0300

    deleted blog pages

--- src/app/(public)/blog/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/(public)/blog/[slug]/page.tsx
-export default function BlogPostPage({ params }: { params: { slug: string } }) {
-    return (
-      <div className="container mx-auto py-10 px-4">
-        <h1 className="text-3xl font-bold">Post: {params.slug}</h1>
-        <p>Content will go here</p>
-      </div>
-    )
-  }
